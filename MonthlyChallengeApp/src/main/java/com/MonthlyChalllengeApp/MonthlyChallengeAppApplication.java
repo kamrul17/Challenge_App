48861b0a3bd04157fc40f46bfa739939ce7e7d8b
kamrul17 <kamrula786@gmail.com>
2024-05-16 13:56:58 +0530

    first commit

---
+++ MonthlyChallengeApp/src/main/java/com/MonthlyChalllengeApp/MonthlyChallengeAppApplication.java
@@ -0,0 +1,13 @@
+package com.MonthlyChalllengeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonthlyChallengeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonthlyChallengeAppApplication.class, args);
+	}
+
+}
---
+++ MonthlyChallengeApp/src/test/java/com/MonthlyChalllengeApp/MonthlyChallengeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MonthlyChalllengeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonthlyChallengeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
